a93d2d03f28b22b9b6457228e51ad25cad2d2e8e
笹倉直哉 <52036434+sasakura870@users.noreply.github.com>
2022-07-19 14:56:44 +0900

    変更

--- Web_GitHub_AutoTest/WeatherForecast.cs
+++ Web_GitHub_AutoTest/WeatherForecast.cs
@@ -6,7 +6,7 @@ namespace Web_GitHub_AutoTest
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5557);
 
         public string? Summary { get; set; }
     }
