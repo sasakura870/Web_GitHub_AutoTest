39d4233399b1774bec8a4ae8da5f9dcd30531bb9
笹倉直哉 <52036434+sasakura870@users.noreply.github.com>
2022-07-19 11:39:37 +0900

    テストプロジェクトを追加

---
+++ Web_Github_AutoTest.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Web_Github_AutoTest.Test
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        public void Test1(int arg)
+        {
+            var hoge = arg * arg;
+            Assert.Equal(Math.Pow(arg, 2), hoge);
+        }
+    }
+}
